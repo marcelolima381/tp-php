5628886a260663d59e644bb8cab4d5e886f979f7
strudel <strudel@Mac-mini-de-COLTEC-Apfel-2.local>
2016-07-15 10:10:05 -0300

    .

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -57,6 +57,46 @@ brworkApp.config(function($routeProvider) {
 		}
 	);
 });
+/**
+ * Serviço para manipulação dos objetos do serviço
+ */
+brworkApp.factory('Service', function($http) {
+  var service = {};
+
+  /**
+   *  Função para tratar GET no serviço
+   */
+  service.get = function(url, callback) {
+    $http.get(url).then(function(response) {
+      var answer = response.data;
+      callback(answer);
+    });
+  };
+
+
+  /**
+   *  Função para tratar POST no serviço
+   */
+  service.post = function(url, data, callback) {
+    $http.post(url, data).then(function(response) {
+      var answer = response.data;
+      callback(answer);
+    });
+  };
+  /**
+   *  Função para tratar POST no serviço
+   */
+  service.delete = function(url, data, callback) {
+    $http.delete(url).then(function(response) {
+      var answer = response.data;
+    });
+  };
+
+  return service;
+});
+
+
+
 // create the controller and inject Angular"s $scope
 brworkApp.controller("mainController", function($scope) {
 
